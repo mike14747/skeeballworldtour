d2ccfc32cafb69d0f22b42fc083093812b0763b4
mike4747 <mike4747@oh.rr.com>
2019-07-29 09:50:55 -0400

    deleted the routes folder since evrything will go through the controllers

--- routes/html/index.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router();
-const homeRoutes = require("./home");
-const standingsRoutes = require("./standings");
-router.use("/", homeRoutes);
-router.use("/", standingsRoutes);
-
-module.exports = router;
