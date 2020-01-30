e329365fd9efd69db4bd6288e44d9bc691c54882
mike14747 <mike4747@oh.rr.com>
2020-01-29 19:24:32 -0500

    added current season view to seasonDropdown and implemented it in the teams page

--- client/src/components/seasonDropdown/seasonDropdown.js
+++ client/src/components/seasonDropdown/seasonDropdown.js
@@ -1,15 +1,50 @@
-import React from 'react';
+// import React from 'react';
+// import './css/seasonDropdown.css';
+// import PropTypes from 'prop-types';
+
+// function Dropdown({ buttonText, listItems, handleSeasonId }) {
+//     return (
+//         <div className="d-flex justify-content-end">
+//             <div className="dropdown mb-3">
+//                 <button className="dropbtn">{buttonText}</button>
+//                 <ul className="dropdown-content">
+//                     {listItems.map(item => (
+//                         <li key={item.season_id} onClick={() => handleSeasonId(item.season_id)}>{item.text}</li>
+//                     ))}
+//                 </ul>
+//             </div>
+//         </div>
+//     );
+// }
+
+// Dropdown.propTypes = {
+//     buttonText: PropTypes.string,
+//     listItems: PropTypes.array,
+//     handleSeasonId: PropTypes.func,
+// };
+
+// export default Dropdown;
+
+import React, { Fragment } from 'react';
 import './css/seasonDropdown.css';
 import PropTypes from 'prop-types';
 
-function Dropdown({ buttonText, listItems, handleSeasonId }) {
+function Dropdown({ currentSeason, buttonText, listItems, handleSeasonId }) {
     return (
         <div className="d-flex justify-content-end">
+            {currentSeason &&
+                <span className="py-1 px-2"><span className="small">Current View:</span> {currentSeason.season_name}, {currentSeason.season_year}</span>
+            }
             <div className="dropdown mb-3">
                 <button className="dropbtn">{buttonText}</button>
                 <ul className="dropdown-content">
                     {listItems.map(item => (
-                        <li key={item.season_id} onClick={() => handleSeasonId(item.season_id)}>{item.text}</li>
+                        <Fragment key={item.season_id}>
+                            {currentSeason && (item.season_id !== currentSeason.season_id)
+                                ? <li onClick={() => handleSeasonId(item.season_id)}>{item.text}</li>
+                                : <li className="viewing">{item.text}</li>
+                            }
+                        </Fragment>
                     ))}
                 </ul>
             </div>
@@ -18,6 +53,7 @@ function Dropdown({ buttonText, listItems, handleSeasonId }) {
 }
 
 Dropdown.propTypes = {
+    currentSeason: PropTypes.object,
     buttonText: PropTypes.string,
     listItems: PropTypes.array,
     handleSeasonId: PropTypes.func,
--- client/src/pages/teams/teams.js
+++ client/src/pages/teams/teams.js
@@ -15,6 +15,9 @@ export default function Teams() {
     const { teamid } = useParams();
     const queryTeamId = teamid || 0;
 
+    const [seasonName, setSeasonName] = useState(null);
+    const [seasonNameStatus, setSeasonNameStatus] = useState({ errorMsg: undefined, isLoaded: false });
+
     const [teamNameStore, setTeamNameStore] = useState(null);
     const [teamNameStoreStatus, setTeamNameStoreStatus] = useState({ errorMsg: undefined, isLoaded: false });
 
@@ -64,12 +67,15 @@ export default function Teams() {
     }, [queryTeamId]);
 
     useEffect(() => {
-        setTeamNameStoreStatus({ errorMsg: undefined, isLoaded: false });
-        setTeamSeasonsStatus({ errorMsg: undefined, isLoaded: false });
-        setTeamStatsStatus({ errorMsg: undefined, isLoaded: false });
-        setTeamScheduleStatus({ errorMsg: undefined, isLoaded: false });
-        setTeamPlayersStatus({ errorMsg: undefined, isLoaded: false });
-        setTeamResultsStatus({ errorMsg: undefined, isLoaded: false });
+        axios.get('/api/seasons/' + querySeasonId + '/name')
+            .then((response) => {
+                response.data[0] ? setSeasonName({ season_id: querySeasonId, season_name: response.data[0].season_name, season_year: response.data[0].year }) : setSeasonName(null);
+            })
+            .catch((error) => {
+                console.log(error);
+                setSeasonName(null);
+                setSeasonNameStatus({ errorMsg: 'An error occurred fetching the current season name/year!', isLoaded: true });
+            });
         axios.get('/api/teams/' + queryTeamId + '/seasons/' + querySeasonId)
             .then((response) => {
                 if (response.data[2][0]) {
@@ -132,7 +138,7 @@ export default function Teams() {
                 </div>
             }
             {teamSeasonsStatus.isLoaded && teamSeasons && teamSeasons.length > 0 &&
-                <SeasonDropdown buttonText="View Stats From:" listItems={teamSeasons} handleSeasonId={handleSeasonId} />
+                <SeasonDropdown currentSeason={seasonName} buttonText="View Stats From:" listItems={teamSeasons} handleSeasonId={handleSeasonId} />
             }
             <div className="row mb-4">
                 <div className="col-sm-6">
