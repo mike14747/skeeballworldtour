3d6d67543cb5956351f2553f5f1093918be14460
mike14747 <mike4747@oh.rr.com>
2020-01-30 22:29:23 -0500

    converted the standings page to use status objects and to show current season view

--- client/src/pages/standings/standings.js
+++ client/src/pages/standings/standings.js
@@ -8,9 +8,14 @@ export default function Standings() {
     const [seasonId, setSeasonId] = useState(null);
     const currentSeasonId = useContext(CurrentSeasonContext);
     const querySeasonId = seasonId || currentSeasonId;
+
+    const [seasonName, setSeasonName] = useState(null);
+
     const [standingsArr, setStandingsArr] = useState();
-    const [areStandingsLoaded, setAreStandingsLoaded] = useState(false);
-    const [standingsSeasons, setStandingsSeasons] = useState([]);
+    const [standingsArrStatus, setStandingsArrStatus] = useState({ errorMsg: undefined, isLoaded: false });
+
+    const [standingsSeasons, setStandingsSeasons] = useState(null);
+    const [standingsSeasonsStatus, setStandingsSeasonsStatus] = useState({ errorMsg: undefined, isLoaded: false });
 
     const handleSeasonId = season => setSeasonId(season);
 
@@ -27,7 +32,7 @@ export default function Standings() {
             standingsArray[index].push(standing);
         });
         setStandingsArr(standingsArray);
-        setAreStandingsLoaded(true);
+        setStandingsArrStatus({ errorMsg: undefined, isLoaded: true });
     }
 
     useEffect(() => {
@@ -40,25 +45,43 @@ export default function Standings() {
                     };
                 });
                 setStandingsSeasons(seasonArray);
+                setStandingsSeasonsStatus({ errorMsg: undefined, isLoaded: true });
             })
-            .catch(err => console.log(err));
+            .catch((error) => {
+                console.log(error);
+                setStandingsSeasons(null);
+                setStandingsSeasonsStatus({ errorMsg: 'An error occurred fetching the standings!', isLoaded: true });
+            });
     }, []);
 
     useEffect(() => {
-        setAreStandingsLoaded(false);
+        axios.get('/api/seasons/' + querySeasonId + '/name')
+            .then((response) => {
+                response.data[0] ? setSeasonName({ season_id: querySeasonId, season_name: response.data[0].season_name, season_year: response.data[0].year }) : setSeasonName(null);
+            })
+            .catch((error) => {
+                console.log(error);
+                setSeasonName(null);
+            });
         axios.get('/api/standings/season/' + querySeasonId)
             .then(response => groupStandings(response.data))
-            .catch(err => console.log(err));
+            .catch((error) => {
+                console.log(error);
+                setStandingsArr(null);
+                setStandingsArrStatus({ errorMsg: 'An error occurred fetching the standings!', isLoaded: true });
+            });
     }, [querySeasonId]);
 
     return (
         <Fragment>
             <PageHeading text="Standings" />
-            {!areStandingsLoaded
+            {standingsSeasonsStatus.isLoaded && standingsSeasons && standingsSeasons.length > 0 &&
+                <SeasonDropdown currentSeason={seasonName} buttonText="View Standings From:" listItems={standingsSeasons} handleSeasonId={handleSeasonId} />
+            }
+            {!standingsArrStatus.isLoaded
                 ? <div className="text-center"><img src={'/images/loading.gif'} alt={'Loading'} /></div>
-                : standingsArr.length > 0
+                : standingsArr && standingsArr.length > 0
                     ? <Fragment>
-                        <SeasonDropdown buttonText="View Standings From:" listItems={standingsSeasons} handleSeasonId={handleSeasonId} />
                         {standingsArr.map((storeDiv, index) => (
                             <div key={index}>
                                 <h5 className="text-center">{storeDiv[0].store_city} - {storeDiv[0].day_name}</h5>
@@ -91,7 +114,9 @@ export default function Standings() {
                             </div>
                         ))}
                     </Fragment>
-                    : <span className="empty-result">There are no standings for this season!</span>
+                    : standingsArr
+                        ? <span className="empty-result">There are no standings for this season!</span>
+                        : <span className="empty-result">{standingsArrStatus.errorMsg}</span>
             }
         </Fragment>
     );
