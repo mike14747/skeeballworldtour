fb3302361e11121261a6fa91576eab231e32a0db
mike14747 <mike4747@oh.rr.com>
2020-02-13 18:52:36 -0500

    updated schedule model and controller to use new try/catch syntax

--- controllers/schedulesController.js
+++ controllers/schedulesController.js
@@ -3,40 +3,37 @@ const Schedule = require('../models/schedule');
 
 router.get('/navbar/:id', async (req, res, next) => {
     try {
-        const data = await Schedule.getCurrentStoresDivisions(req.params.id);
-        res.json(data);
-    } catch (err) {
-        console.log('An error has occurred! ' + err);
-        res.status(500).send('Request failed... please check your request and try again!');
+        const data = await Schedule.getCurrentStoresDivisions({
+            id: req.params.id,
+        });
+        data[0] ? res.json(data[1]) : next(data[1]);
+    } catch (error) {
+        next(error);
     }
 });
 
 router.get('/store/:storeid/division/:divisionid/seasons-list', async (req, res, next) => {
-    const paramsObj = {
-        store_id: req.params.storeid,
-        division_id: req.params.divisionid,
-    };
     try {
-        const data = await Schedule.getSeasonsList(paramsObj);
-        res.json(data);
-    } catch (err) {
-        console.log('An error has occurred! ' + err);
-        res.status(500).send('Request failed... please check your request and try again!');
+        const data = await Schedule.getSeasonsList({
+            store_id: req.params.storeid,
+            division_id: req.params.divisionid,
+        });
+        data[0] ? res.json(data[1]) : next(data[1]);
+    } catch (error) {
+        next(error);
     }
 });
 
 router.get('/store/:storeid/division/:divisionid/season/:seasonid', async (req, res, next) => {
-    const paramsObj = {
-        store_id: req.params.storeid,
-        division_id: req.params.divisionid,
-        season_id: req.params.seasonid,
-    };
     try {
-        const data = await Schedule.getSchedulesByStoreDivisionSeason(paramsObj);
-        res.json(data);
-    } catch (err) {
-        console.log('An error has occurred! ' + err);
-        res.status(500).send('Request failed... please check your request and try again!');
+        const data = await Schedule.getSchedulesByStoreDivisionSeason({
+            store_id: req.params.storeid,
+            division_id: req.params.divisionid,
+            season_id: req.params.seasonid,
+        });
+        data[0] ? res.json(data[1]) : next(data[1]);
+    } catch (error) {
+        next(error);
     }
 });
 
--- models/schedule.js
+++ models/schedule.js
@@ -1,23 +1,43 @@
 const pool = require('../config/pool.js');
 
 const Schedule = {
-    getCurrentStoresDivisions: async (id) => {
-        const queryString = 'SELECT DISTINCT sc.division_id, d.day_name, s.store_id, s.store_city, CONCAT(s.store_id,sc.division_id) AS store_division FROM schedule AS sc JOIN divisions AS d ON (sc.division_id=d.division_id) JOIN stores AS s ON (sc.store_id=s.store_id) WHERE sc.season_id=? && s.active=1 ORDER BY s.store_city, d.division_id ASC;';
-        const queryParams = [id];
-        const [result] = await pool.query(queryString, queryParams);
-        return result;
+    getCurrentStoresDivisions: async (paramsObj) => {
+        try {
+            const queryString = 'SELECT DISTINCT sc.division_id, d.day_name, s.store_id, s.store_city, CONCAT(s.store_id,sc.division_id) AS store_division FROM schedule AS sc JOIN divisions AS d ON (sc.division_id=d.division_id) JOIN stores AS s ON (sc.store_id=s.store_id) WHERE sc.season_id=? && s.active=1 ORDER BY s.store_city, d.division_id ASC;';
+            const queryParams = [
+                paramsObj.id,
+            ];
+            const [result] = await pool.query(queryString, queryParams);
+            return [true, result];
+        } catch (error) {
+            return [false, error];
+        }
     },
     getSeasonsList: async (paramsObj) => {
-        const queryString = 'SELECT DISTINCT(sc.season_id), se.season_id, se.season_name, se.year FROM schedule AS sc JOIN seasons AS se ON (sc.season_id=se.season_id) WHERE sc.store_id=? && sc.division_id=? ORDER BY se.season_id DESC;';
-        const queryParams = [paramsObj.store_id, paramsObj.division_id];
-        const [result] = await pool.query(queryString, queryParams);
-        return result;
+        try {
+            const queryString = 'SELECT DISTINCT(sc.season_id), se.season_id, se.season_name, se.year FROM schedule AS sc JOIN seasons AS se ON (sc.season_id=se.season_id) WHERE sc.store_id=? && sc.division_id=? ORDER BY se.season_id DESC;';
+            const queryParams = [
+                paramsObj.store_id,
+                paramsObj.division_id,
+            ];
+            const [result] = await pool.query(queryString, queryParams);
+            return [true, result];
+        } catch (error) {
+            return [false, error];
+        }
     },
     getSchedulesByStoreDivisionSeason: async (paramsObj) => {
-        const queryString = 'SELECT ds.week_id, DATE_FORMAT(ds.week_date, "%M %d, %Y") AS week_date1, ds.away_team_id, (SELECT t.team_name FROM teams AS t WHERE t.team_id=ds.away_team_id) AS away_team, ds.home_team_id, (SELECT t.team_name FROM teams AS t WHERE t.team_id=ds.home_team_id) AS home_team, ds.alley, ds.start_time FROM (SELECT sc.week_id, sc.week_date, sc.away_team_id AS away_team_id, sc.home_team_id AS home_team_id, sc.alley AS alley, sc.start_time AS start_time FROM schedule AS sc WHERE sc.store_id=? && sc.division_id=? && sc.season_id=? ORDER BY sc.week_id ASC, sc.start_time ASC, sc.alley ASC) AS ds JOIN teams AS t ON (ds.away_team_id=t.team_id) ORDER BY ds.week_id ASC, ds.start_time ASC, ds.alley ASC;';
-        const queryParams = [paramsObj.store_id, paramsObj.division_id, paramsObj.season_id];
-        const [result] = await pool.query(queryString, queryParams);
-        return result;
+        try {
+            const queryString = 'SELECT ds.week_id, DATE_FORMAT(ds.week_date, "%M %d, %Y") AS week_date1, ds.away_team_id, (SELECT t.team_name FROM teams AS t WHERE t.team_id=ds.away_team_id) AS away_team, ds.home_team_id, (SELECT t.team_name FROM teams AS t WHERE t.team_id=ds.home_team_id) AS home_team, ds.alley, ds.start_time FROM (SELECT sc.week_id, sc.week_date, sc.away_team_id AS away_team_id, sc.home_team_id AS home_team_id, sc.alley AS alley, sc.start_time AS start_time FROM schedule AS sc WHERE sc.store_id=? && sc.division_id=? && sc.season_id=? ORDER BY sc.week_id ASC, sc.start_time ASC, sc.alley ASC) AS ds JOIN teams AS t ON (ds.away_team_id=t.team_id) ORDER BY ds.week_id ASC, ds.start_time ASC, ds.alley ASC;';
+            const queryParams = [
+                paramsObj.store_id,
+                paramsObj.division_id, paramsObj.season_id,
+            ];
+            const [result] = await pool.query(queryString, queryParams);
+            return [true, result];
+        } catch (error) {
+            return [false, error];
+        }
     },
 };
 
