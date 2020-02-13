2d02d06c4ee4d765575269a221c16bc50a2eb7ed
mike14747 <mike4747@oh.rr.com>
2020-02-13 18:41:58 -0500

    updated results model and controller to use new try/catch syntax

--- controllers/resultsController.js
+++ controllers/resultsController.js
@@ -2,31 +2,27 @@ const router = require('express').Router();
 const Result = require('../models/result');
 
 router.get('/store/:storeid/division/:divisionid/season/:seasonid', async (req, res, next) => {
-    const paramsObj = {
-        store_id: req.params.storeid,
-        division_id: req.params.divisionid,
-        season_id: req.params.seasonid,
-    };
     try {
-        const data = await Result.getResultsByStoreDivisionSeason(paramsObj);
-        res.json(data);
-    } catch (err) {
-        console.log('An error has occurred! ' + err);
-        res.status(500).send('Request failed... please check your request and try again!');
+        const data = await Result.getResultsByStoreDivisionSeason({
+            store_id: req.params.storeid,
+            division_id: req.params.divisionid,
+            season_id: req.params.seasonid,
+        });
+        data[0] ? res.json(data[1]) : next(data[1]);
+    } catch (error) {
+        next(error);
     }
 });
 
 router.get('/store/:storeid/division/:divisionid/seasons-list', async (req, res, next) => {
-    const paramsObj = {
-        store_id: req.params.storeid,
-        division_id: req.params.divisionid,
-    };
     try {
-        const data = await Result.getSeasonsList(paramsObj);
-        res.json(data);
-    } catch (err) {
-        console.log('An error has occurred! ' + err);
-        res.status(500).send('Request failed... please check your request and try again!');
+        const data = await Result.getSeasonsList({
+            store_id: req.params.storeid,
+            division_id: req.params.divisionid,
+        });
+        data[0] ? res.json(data[1]) : next(data[1]);
+    } catch (error) {
+        next(error);
     }
 });
 
