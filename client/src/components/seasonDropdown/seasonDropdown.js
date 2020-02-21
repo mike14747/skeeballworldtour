a7d16efa746ac944e45730bc025cfa8becf301d7
mike14747 <mike4747@oh.rr.com>
2020-02-20 19:37:59 -0500

    removed some comented out code from the seasonDropdown component

--- client/src/components/seasonDropdown/seasonDropdown.js
+++ client/src/components/seasonDropdown/seasonDropdown.js
@@ -1,30 +1,3 @@
-// import React from 'react';
-// import './css/seasonDropdown.css';
-// import PropTypes from 'prop-types';
-
-// function Dropdown({ buttonText, listItems, handleSeasonId }) {
-//     return (
-//         <div className="d-flex justify-content-end">
-//             <div className="dropdown mb-3">
-//                 <button className="dropbtn">{buttonText}</button>
-//                 <ul className="dropdown-content">
-//                     {listItems.map(item => (
-//                         <li key={item.season_id} onClick={() => handleSeasonId(item.season_id)}>{item.text}</li>
-//                     ))}
-//                 </ul>
-//             </div>
-//         </div>
-//     );
-// }
-
-// Dropdown.propTypes = {
-//     buttonText: PropTypes.string,
-//     listItems: PropTypes.array,
-//     handleSeasonId: PropTypes.func,
-// };
-
-// export default Dropdown;
-
 import React, { Fragment } from 'react';
 import './css/seasonDropdown.css';
 import PropTypes from 'prop-types';
