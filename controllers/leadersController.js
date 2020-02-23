1a5fe5efb16a9b803a5997133d252c2970255d1a
mike14747 <mike4747@oh.rr.com>
2020-02-22 23:06:29 -0500

    individual one-game and ten-game leaders are now working with the ifnull condition on tie values

--- controllers/leadersController.js
+++ controllers/leadersController.js
@@ -25,6 +25,20 @@ router.get('/individual/one-game/:seasonid/:numleaders', async (req, res, next) 
     }
 });
 
+router.get('/individual/one-game/:seasonid/:numleaders/store/:storeid/division/:divisionid', async (req, res, next) => {
+    try {
+        const data = await Leader.getIndividualOneGameBySeasonStoreDivisionId({
+            season_id: Number(req.params.seasonid),
+            num_leaders: Number(req.params.numleaders),
+            store_id: Number(req.params.storeid),
+            division_id: Number(req.params.divisionid),
+        });
+        data[0] ? res.json([data[1][2], data[1][3]]) : next(data[1]);
+    } catch (error) {
+        next(error);
+    }
+});
+
 router.get('/individual/ten-game/:seasonid/:numleaders', async (req, res, next) => {
     try {
         const data = await Leader.getIndividualTenGameBySeasonId({
