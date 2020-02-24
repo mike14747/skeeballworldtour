7c1deff6286ed1452da9f6d62db0513dcce3ba1f
mike14747 <mike4747@oh.rr.com>
2020-02-23 23:51:13 -0500

    team average leaders are now all working

--- models/leader.js
+++ models/leader.js
@@ -100,6 +100,7 @@ const Leader = {
     getTeamAverage: async (paramsObj) => {
         try {
             let selectClause = '';
+            let selectClause2 = '';
             let setVariables = '';
             const queryParams = [
                 paramsObj.num_leaders - 1,
@@ -107,30 +108,35 @@ const Leader = {
             ];
             if (paramsObj.season_id && paramsObj.store_id && paramsObj.division_id) {
                 if (paramsObj.season_id > 5) {
-                    selectClause = '';
+                    selectClause = 'SELECT ROUND(10*total_points/(wins+losses+ties),5) AS data FROM standings WHERE season_id=@season_id && store_id=@store_id && division_id=@division_id';
+                    selectClause2 = 'SELECT team_id, store_id, ROUND(10*total_points/(wins+losses+ties),5) AS data FROM standings WHERE season_id=@season_id && store_id=@store_id && division_id=@division_id HAVING data>@tie_value_num_leaders_plus_one';
                 } else {
-                    selectClause = '';
+                    selectClause = 'SELECT ROUND(total_points/(wins+losses+ties),5) AS data FROM standings WHERE season_id=@season_id && store_id=@store_id && division_id=@division_id';
+                    selectClause2 = 'SELECT team_id, store_id, ROUND(total_points/(wins+losses+ties),5) AS data FROM standings WHERE season_id=@season_id && store_id=@store_id && division_id=@division_id HAVING data>@tie_value_num_leaders_plus_one';
                 }
                 setVariables = 'SET @season_id=?, @store_id=?, @division_id=?;';
                 queryParams.unshift(paramsObj.season_id, paramsObj.store_id, paramsObj.division_id);
             } else if (paramsObj.season_id) {
                 if (paramsObj.season_id > 5) {
-                    selectClause = '';
+                    selectClause = 'SELECT ROUND(10*total_points/(wins+losses+ties),5) AS data FROM standings WHERE season_id=@season_id';
+                    selectClause2 = 'SELECT team_id, store_id, ROUND(10*total_points/(wins+losses+ties),5) AS data FROM standings WHERE season_id=@season_id HAVING data>@tie_value_num_leaders_plus_one';
                 } else {
-                    selectClause = '';
+                    selectClause = 'SELECT ROUND(total_points/(wins+losses+ties),5) AS data FROM standings WHERE season_id=@season_id';
+                    selectClause2 = 'SELECT team_id, store_id, ROUND(total_points/(wins+losses+ties),5) AS data FROM standings WHERE season_id=@season_id HAVING data>@tie_value_num_leaders_plus_one';
                 }
                 setVariables = 'SET @season_id=?;';
                 queryParams.unshift(paramsObj.season_id);
             } else {
                 selectClause = 'SELECT ROUND(10*total_points/(wins+losses+ties),5) AS data FROM standings WHERE season_id>5 UNION ALL SELECT ROUND(total_points/(wins+losses+ties),5) AS data FROM standings WHERE season_id<=5';
+                selectClause2 = 'SELECT team_id, store_id, ROUND(10*total_points/(wins+losses+ties),5) AS data FROM standings WHERE season_id>5 HAVING data>@tie_value_num_leaders_plus_one UNION ALL SELECT team_id, store_id, ROUND(total_points/(wins+losses+ties),5) AS data FROM standings WHERE season_id<=5 HAVING data>@tie_value_num_leaders_plus_one';
                 setVariables = 'SET @dummy_variable=?;';
                 queryParams.unshift(0);
             }
             const setTieValueVariables = 'SELECT r1.data, r2.data INTO @tie_value_num_leaders, @tie_value_num_leaders_plus_one FROM (SELECT IFNULL((' + selectClause + ' ORDER BY data DESC LIMIT ?, 1), 1) AS data) AS r1, (SELECT IFNULL((' + selectClause + ' ORDER BY data DESC LIMIT ?, 1), 0) AS data) AS r2;';
-            const tiesInfo = 'SELECT (CASE WHEN @tie_value_num_leaders=@tie_value_num_leaders_plus_one THEN (SELECT COUNT(*) FROM (SELECT ROUND(10*total_points/(wins+losses+ties),5) AS data FROM standings WHERE season_id>5 HAVING data=@tie_value_num_leaders_plus_one UNION ALL SELECT ROUND(total_points/(wins+losses+ties),5) AS data FROM standings WHERE season_id<=5 HAVING data=@tie_value_num_leaders_plus_one) AS nt) ELSE 1 END) AS num_at_tie_value, @tie_value_num_leaders AS tie_value;';
-            const mainQuery = 'SELECT t.team_id AS field_id, t.team_name AS field_name, s.store_city, st.data FROM (SELECT team_id, store_id, ROUND(10*total_points/(wins+losses+ties),5) AS data FROM standings WHERE season_id>5 HAVING data>@tie_value_num_leaders_plus_one UNION ALL SELECT team_id, store_id, ROUND(total_points/(wins+losses+ties),5) AS data FROM standings WHERE season_id<=5 HAVING data>@tie_value_num_leaders_plus_one) as st JOIN teams AS t USING (team_id) JOIN stores AS s ON (st.store_id=s.store_id) ORDER BY st.data DESC;';
+            const tiesInfo = 'SELECT (CASE WHEN @tie_value_num_leaders=@tie_value_num_leaders_plus_one THEN (SELECT COUNT(*) FROM (' + selectClause + ' HAVING data=@tie_value_num_leaders_plus_one) AS nt) ELSE 1 END) AS num_at_tie_value, @tie_value_num_leaders AS tie_value;';
+            const mainQuery = 'SELECT t.team_id AS field_id, t.team_name AS field_name, s.store_city, st.data FROM (' + selectClause2 + ') as st JOIN teams AS t USING (team_id) JOIN stores AS s ON (st.store_id=s.store_id) ORDER BY st.data DESC;';
             const queryString = setVariables + setTieValueVariables + tiesInfo + mainQuery;
-            console.log(queryString);
+            // console.log(queryString);
             const [result] = await pool.query(queryString, queryParams);
             return [true, result];
         } catch (error) {
@@ -154,25 +160,6 @@ const Leader = {
             return [false, error];
         }
     },
-    getTeamOneGameBySeasonStoreDivisionId: async (paramsObj) => {
-        try {
-            const setTieValueVariables = 'SELECT r1.data, r2.data INTO @tie_value_num_leaders, @tie_value_num_leaders_plus_one FROM (SELECT IFNULL((SELECT tv1.data FROM (SELECT SUM(g1) AS data FROM results WHERE season_id=@season_id && store_id=@store_id && division_id=@division_id GROUP BY team_id, week_id UNION ALL SELECT SUM(g2) AS data FROM results WHERE season_id=@season_id && store_id=@store_id && division_id=@division_id GROUP BY team_id, week_id UNION ALL SELECT SUM(g3) AS data FROM results WHERE season_id=@season_id && store_id=@store_id && division_id=@division_id GROUP BY team_id, week_id UNION ALL SELECT SUM(g4) AS data FROM results WHERE season_id=@season_id && store_id=@store_id && division_id=@division_id GROUP BY team_id, week_id UNION ALL SELECT SUM(g5) AS data FROM results WHERE season_id=@season_id && store_id=@store_id && division_id=@division_id GROUP BY team_id, week_id UNION ALL SELECT SUM(g6) AS data FROM results WHERE season_id=@season_id && store_id=@store_id && division_id=@division_id GROUP BY team_id, week_id UNION ALL SELECT SUM(g7) AS data FROM results WHERE season_id=@season_id && store_id=@store_id && division_id=@division_id GROUP BY team_id, week_id UNION ALL SELECT SUM(g8) AS data FROM results WHERE season_id=@season_id && store_id=@store_id && division_id=@division_id GROUP BY team_id, week_id UNION ALL SELECT SUM(g9) AS data FROM results WHERE season_id=@season_id && store_id=@store_id && division_id=@division_id GROUP BY team_id, week_id UNION ALL SELECT SUM(g10) AS data FROM results WHERE season_id=@season_id && store_id=@store_id && division_id=@division_id GROUP BY team_id, week_id) AS tv1 ORDER BY data DESC LIMIT ?, 1), 1) AS data) AS r1, (SELECT IFNULL((SELECT tv1.data FROM (SELECT SUM(g1) AS data FROM results WHERE season_id=@season_id && store_id=@store_id && division_id=@division_id GROUP BY team_id, week_id UNION ALL SELECT SUM(g2) AS data FROM results WHERE season_id=@season_id && store_id=@store_id && division_id=@division_id GROUP BY team_id, week_id UNION ALL SELECT SUM(g3) AS data FROM results WHERE season_id=@season_id && store_id=@store_id && division_id=@division_id GROUP BY team_id, week_id UNION ALL SELECT SUM(g4) AS data FROM results WHERE season_id=@season_id && store_id=@store_id && division_id=@division_id GROUP BY team_id, week_id UNION ALL SELECT SUM(g5) AS data FROM results WHERE season_id=@season_id && store_id=@store_id && division_id=@division_id GROUP BY team_id, week_id UNION ALL SELECT SUM(g6) AS data FROM results WHERE season_id=@season_id && store_id=@store_id && division_id=@division_id GROUP BY team_id, week_id UNION ALL SELECT SUM(g7) AS data FROM results WHERE season_id=@season_id && store_id=@store_id && division_id=@division_id GROUP BY team_id, week_id UNION ALL SELECT SUM(g8) AS data FROM results WHERE season_id=@season_id && store_id=@store_id && division_id=@division_id GROUP BY team_id, week_id UNION ALL SELECT SUM(g9) AS data FROM results WHERE season_id=@season_id && store_id=@store_id && division_id=@division_id GROUP BY team_id, week_id UNION ALL SELECT SUM(g10) AS data FROM results WHERE season_id=@season_id && store_id=@store_id && division_id=@division_id GROUP BY team_id, week_id) AS tv1 ORDER BY data DESC LIMIT ?, 1), 0) AS data) AS r2;';
-            const tiesInfo = 'SELECT (CASE WHEN @tie_value_num_leaders=@tie_value_num_leaders_plus_one THEN (SELECT COUNT(*) FROM ((SELECT SUM(g1) AS data FROM results WHERE season_id=@season_id && store_id=@store_id && division_id=@division_id GROUP BY team_id, week_id UNION ALL SELECT SUM(g2) AS data FROM results WHERE season_id=@season_id && store_id=@store_id && division_id=@division_id GROUP BY team_id, week_id UNION ALL SELECT SUM(g3) AS data FROM results WHERE season_id=@season_id && store_id=@store_id && division_id=@division_id GROUP BY team_id, week_id UNION ALL SELECT SUM(g4) AS data FROM results WHERE season_id=@season_id && store_id=@store_id && division_id=@division_id GROUP BY team_id, week_id UNION ALL SELECT SUM(g5) AS data FROM results WHERE season_id=@season_id && store_id=@store_id && division_id=@division_id GROUP BY team_id, week_id UNION ALL SELECT SUM(g6) AS data FROM results WHERE season_id=@season_id && store_id=@store_id && division_id=@division_id GROUP BY team_id, week_id UNION ALL SELECT SUM(g7) AS data FROM results WHERE season_id=@season_id && store_id=@store_id && division_id=@division_id GROUP BY team_id, week_id UNION ALL SELECT SUM(g8) AS data FROM results WHERE season_id=@season_id && store_id=@store_id && division_id=@division_id GROUP BY team_id, week_id UNION ALL SELECT SUM(g9) AS data FROM results WHERE season_id=@season_id && store_id=@store_id && division_id=@division_id GROUP BY team_id, week_id UNION ALL SELECT SUM(g10) AS data FROM results WHERE season_id=@season_id && store_id=@store_id && division_id=@division_id GROUP BY team_id, week_id) AS ti) WHERE ti.data=@tie_value_num_leaders_plus_one) ELSE 1 END) AS num_at_tie_value, @tie_value_num_leaders AS tie_value;';
-            const mainQuery = 'SELECT t.team_id AS field_id, t.team_name AS field_name, st.store_city, r.data FROM (SELECT ti.team_id, ti.data FROM (SELECT team_id, SUM(g1) AS data FROM results WHERE season_id=@season_id && store_id=@store_id && division_id=@division_id GROUP BY team_id, week_id UNION ALL SELECT team_id, SUM(g2) AS data FROM results WHERE season_id=@season_id && store_id=@store_id && division_id=@division_id GROUP BY team_id, week_id UNION ALL SELECT team_id, SUM(g3) AS data FROM results WHERE season_id=@season_id && store_id=@store_id && division_id=@division_id GROUP BY team_id, week_id UNION ALL SELECT team_id, SUM(g4) AS data FROM results WHERE season_id=@season_id && store_id=@store_id && division_id=@division_id GROUP BY team_id, week_id UNION ALL SELECT team_id, SUM(g5) AS data FROM results WHERE season_id=@season_id && store_id=@store_id && division_id=@division_id GROUP BY team_id, week_id UNION ALL SELECT team_id, SUM(g6) AS data FROM results WHERE season_id=@season_id && store_id=@store_id && division_id=@division_id GROUP BY team_id, week_id UNION ALL SELECT team_id, SUM(g7) AS data FROM results WHERE season_id=@season_id && store_id=@store_id && division_id=@division_id GROUP BY team_id, week_id UNION ALL SELECT team_id, SUM(g8) AS data FROM results WHERE season_id=@season_id && store_id=@store_id && division_id=@division_id GROUP BY team_id, week_id UNION ALL SELECT team_id, SUM(g9) AS data FROM results WHERE season_id=@season_id && store_id=@store_id && division_id=@division_id GROUP BY team_id, week_id UNION ALL SELECT team_id, SUM(g10) AS data FROM results WHERE season_id=@season_id && store_id=@store_id && division_id=@division_id GROUP BY team_id, week_id) AS ti WHERE ti.data>@tie_value_num_leaders_plus_one) AS r INNER JOIN teams AS t ON (r.team_id=t.team_id) INNER JOIN stores AS st ON (t.store_id=st.store_id) ORDER BY data DESC;';
-            const queryString = 'SET @season_id=?, @store_id=?, @division_id=?;' + setTieValueVariables + tiesInfo + mainQuery;
-            const queryParams = [
-                paramsObj.season_id,
-                paramsObj.store_id,
-                paramsObj.division_id,
-                paramsObj.num_leaders - 1,
-                paramsObj.num_leaders,
-            ];
-            const [result] = await pool.query(queryString, queryParams);
-            return [true, result];
-        } catch (error) {
-            return [false, error];
-        }
-    },
     getTeamTenGameBySeasonId: async (paramsObj) => {
         try {
             const setTieValueVariables = 'SELECT r1.data, r2.data INTO @tie_value_num_leaders, @tie_value_num_leaders_plus_one FROM (SELECT IFNULL((SELECT (SUM(g1)+SUM(g2)+SUM(g3)+SUM(g4)+SUM(g5)+SUM(g6)+SUM(g7)+SUM(g8)+SUM(g9)+SUM(g10)) as data FROM results WHERE season_id=@season_id GROUP BY team_id, week_id ORDER BY data DESC LIMIT ?, 1), 1) AS data) AS r1, (SELECT IFNULL((SELECT (SUM(g1)+SUM(g2)+SUM(g3)+SUM(g4)+SUM(g5)+SUM(g6)+SUM(g7)+SUM(g8)+SUM(g9)+SUM(g10)) as data FROM results WHERE season_id=@season_id GROUP BY team_id, week_id ORDER BY data DESC LIMIT ?, 1), 0) AS data) AS r2;';
@@ -181,25 +168,6 @@ const Leader = {
             const queryString = 'SET @season_id=?;' + setTieValueVariables + tiesInfo + mainQuery;
             const queryParams = [
                 paramsObj.season_id,
-                paramsObj.num_leaders - 1,
-                paramsObj.num_leaders,
-            ];
-            const [result] = await pool.query(queryString, queryParams);
-            return [true, result];
-        } catch (error) {
-            return [false, error];
-        }
-    },
-    getTeamTenGameBySeasonStoreDivisionId: async (paramsObj) => {
-        try {
-            const setTieValueVariables = 'SELECT r1.data, r2.data INTO @tie_value_num_leaders, @tie_value_num_leaders_plus_one FROM (SELECT IFNULL((SELECT (SUM(g1)+SUM(g2)+SUM(g3)+SUM(g4)+SUM(g5)+SUM(g6)+SUM(g7)+SUM(g8)+SUM(g9)+SUM(g10)) as data FROM results WHERE season_id=@season_id && store_id=@store_id && division_id=@division_id GROUP BY team_id, week_id ORDER BY data DESC LIMIT ?, 1), 1) AS data) AS r1, (SELECT IFNULL((SELECT (SUM(g1)+SUM(g2)+SUM(g3)+SUM(g4)+SUM(g5)+SUM(g6)+SUM(g7)+SUM(g8)+SUM(g9)+SUM(g10)) as data FROM results WHERE season_id=@season_id && store_id=@store_id && division_id=@division_id GROUP BY team_id, week_id ORDER BY data DESC LIMIT ?, 1), 0) AS data) AS r2;';
-            const tiesInfo = 'SELECT (CASE WHEN @tie_value_num_leaders=@tie_value_num_leaders_plus_one THEN (SELECT COUNT(*) FROM (SELECT (SUM(g1)+SUM(g2)+SUM(g3)+SUM(g4)+SUM(g5)+SUM(g6)+SUM(g7)+SUM(g8)+SUM(g9)+SUM(g10)) as data FROM results WHERE season_id=@season_id && store_id=@store_id && division_id=@division_id GROUP BY team_id, week_id HAVING data=@tie_value_num_leaders_plus_one ORDER BY data DESC) AS nt) ELSE 1 END) AS num_at_tie_value, @tie_value_num_leaders AS tie_value;';
-            const mainQuery = 'SELECT t.team_id AS field_id, t.team_name AS field_name, st.store_city, (SUM(r.g1)+SUM(r.g2)+SUM(r.g3)+SUM(r.g4)+SUM(r.g5)+SUM(r.g6)+SUM(r.g7)+SUM(r.g8)+SUM(r.g9)+SUM(r.g10)) as data FROM results AS r INNER JOIN teams AS t ON (r.team_id=t.team_id) INNER JOIN stores AS st ON (t.store_id=st.store_id) WHERE r.season_id=@season_id && r.store_id=@store_id && r.division_id=@division_id GROUP BY r.team_id, r.week_id HAVING data>@tie_value_num_leaders_plus_one ORDER BY data DESC;';
-            const queryString = 'SET @season_id=?, @store_id=?, @division_id=?;' + setTieValueVariables + tiesInfo + mainQuery;
-            const queryParams = [
-                paramsObj.season_id,
-                paramsObj.store_id,
-                paramsObj.division_id,
                 paramsObj.num_leaders - 1,
                 paramsObj.num_leaders,
             ];
