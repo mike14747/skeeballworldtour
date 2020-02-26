bdb8990d591d837e8c16620c2386c7c794bec9f6
mike14747 <mike4747@oh.rr.com>
2020-02-25 23:17:46 -0500

    added enough content to the allTime page to get rid of the warnings in the console

--- client/src/pages/alltime/alltime.js
+++ client/src/pages/alltime/alltime.js
@@ -1,10 +1,30 @@
 import React, { useState, useEffect, Fragment } from 'react';
+import PageHeading from '../../components/pageHeading/pageHeading';
+import NumLeadersDropdown from '../../components/numLeadersDropdown/numLeadersDropdown';
 
 const AllTime = () => {
+    const [numLeaders, setNumLeaders] = useState(20);
+    const numLeadersArray = [10, 20, 50, 100];
+
+    useEffect(() => {
+
+    }, [numLeaders]);
+
     return (
-        <div>
-            This will become the all-time leaders page!
-        </div>
+        <Fragment>
+            <PageHeading text="League Leaders" />
+            <div className="row mb-4">
+                <div className="col-4 text-left p-2">
+
+                </div>
+                <div className="col-4 text-center p-2">
+                    <NumLeadersDropdown numLeaders={numLeaders} numLeadersArray={numLeadersArray} setNumLeaders={setNumLeaders} />
+                </div>
+                <div className="col-4 text-right p-2">
+
+                </div>
+            </div>
+        </Fragment>
     );
 };
 
