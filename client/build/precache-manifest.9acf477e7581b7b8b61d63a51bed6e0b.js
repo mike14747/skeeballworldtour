44f8adfb5bca028ab12f8d89dff7a95e55c47fae
mike14747 <mike4747@oh.rr.com>
2020-03-03 23:09:24 -0500

    removed the client build folder from client/.gitignore file, so hopefully it'll get push to github

---
+++ client/build/precache-manifest.9acf477e7581b7b8b61d63a51bed6e0b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "852f6ccd863b7bc0fd40e6cdca296c94",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ad41f2476319ed961ad7",
+    "url": "/static/css/main.8c47a056.chunk.css"
+  },
+  {
+    "revision": "6b02dc3d3d09d01852f3",
+    "url": "/static/js/2.dc7c861f.chunk.js"
+  },
+  {
+    "revision": "6d397f79152aafaf77de9b10c1012183",
+    "url": "/static/js/2.dc7c861f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ad41f2476319ed961ad7",
+    "url": "/static/js/main.421e8694.chunk.js"
+  },
+  {
+    "revision": "96993f09dd8dfd383718",
+    "url": "/static/js/runtime-main.4e03d699.js"
+  },
+  {
+    "revision": "0a0d1a1f467dad40c6979e69092b4172",
+    "url": "/static/media/head_bg_new.0a0d1a1f.jpg"
+  },
+  {
+    "revision": "102ce1b823121b20a56ca0ee8ba83cbe",
+    "url": "/static/media/skeeball_logo.102ce1b8.png"
+  }
+]);
