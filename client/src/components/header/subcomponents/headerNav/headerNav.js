3db9c06db8ea57f17a33f19618645e1e0bc4e761
mike14747 <mike4747@oh.rr.com>
2020-03-16 20:01:57 -0400

    headerNav and its css file seem to be finished

--- client/src/components/header/subcomponents/headerNav/headerNav.js
+++ client/src/components/header/subcomponents/headerNav/headerNav.js
@@ -3,23 +3,13 @@ import './css/headerNav.css';
 
 const HeaderNav = () => {
     return (
-        // <div className="h-nav">
-        //     <label htmlFor="toggle">&#9776;</label>
-        //     <input type="checkbox" id="toggle" />
-        //     <div className="menu">
-        //         <a href="/champions">Champions</a>
-        //         <a href="/rules">Rules</a>
-        //         <a href="/leaders">Leaders</a>
-        //         <a href="/all-time">All-Time Records</a>
-        //     </div>
-        // </div>
-        <div className="h-nav ml-2">
-            <button className="h-nav-btn">&#9776;</button>
+        <div className="h-nav-container">
+            <span className="h-nav-burger">&#9776;</span>
             <ul className="h-nav-content">
-                <a href="/champions"><li className="current">Champions</li></a>
-                <a href="/rules"><li className="current">Rules</li></a>
-                <a href="/leaders"><li className="current">Leaders</li></a>
-                <a href="/all-time"><li className="current">All-Time Records</li></a>
+                <a href="/champions"><li className="nav-item">Champions</li></a>
+                <a href="/rules"><li className="nav-item">Rules</li></a>
+                <a href="/leaders"><li className="nav-item">Leaders</li></a>
+                <a href="/all-time"><li className="nav-item">All-Time Records</li></a>
             </ul>
         </div>
     );
