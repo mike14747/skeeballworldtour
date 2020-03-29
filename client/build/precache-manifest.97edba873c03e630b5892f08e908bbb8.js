90c294689f3695bfdc3724a6b498ef8d9bdc31fe
mike14747 <mike4747@oh.rr.com>
2020-03-29 00:16:23 -0400

    ran a new build to deploy

---
+++ client/build/precache-manifest.97edba873c03e630b5892f08e908bbb8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b692cb531e54b0cb79511ee075850ca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "71030be1e5c11f3ef56a",
+    "url": "/static/css/main.db5c62f3.chunk.css"
+  },
+  {
+    "revision": "a9d38e47389fad956068",
+    "url": "/static/js/2.7e9f2da1.chunk.js"
+  },
+  {
+    "revision": "0b08544f54c9906705be39c03b7e79ad",
+    "url": "/static/js/2.7e9f2da1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71030be1e5c11f3ef56a",
+    "url": "/static/js/main.29c6bdb2.chunk.js"
+  },
+  {
+    "revision": "e8f761f6bc24c5f8e3b0",
+    "url": "/static/js/runtime-main.6f9a79eb.js"
+  },
+  {
+    "revision": "0a0d1a1f467dad40c6979e69092b4172",
+    "url": "/static/media/head_bg_new.0a0d1a1f.jpg"
+  },
+  {
+    "revision": "102ce1b823121b20a56ca0ee8ba83cbe",
+    "url": "/static/media/skeeball_logo.102ce1b8.png"
+  }
+]);
