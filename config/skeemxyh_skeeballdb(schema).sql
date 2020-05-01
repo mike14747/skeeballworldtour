030127174404dc096f504f750611349ca92dcd30
mike14747 <mike4747@oh.rr.com>
2020-04-30 22:24:21 -0400

    added passport, passport-local, express -session, express-mysql-session and initialized them in server.js, installed another react frontend in /admin and it is working

--- config/skeemxyh_skeeballdb(schema).sql
+++ config/skeemxyh_skeeballdb(schema).sql
@@ -204,4 +204,13 @@ CREATE TABLE users (
 
 -- --------------------------------------------------------
 
+CREATE TABLE sessions (
+    session_id varchar(128) COLLATE utf8mb4_bin NOT NULL,
+    expires int(11) unsigned NOT NULL,
+    data mediumtext COLLATE utf8mb4_bin,
+    PRIMARY KEY (session_id)
+);
+
+-- --------------------------------------------------------
+
 set foreign_key_checks=1;
--- server.js
+++ server.js
@@ -7,8 +7,25 @@ const path = require('path');
 
 const connectionPool = require('./config/connectionPool');
 
+const passport = require('passport');
+const session = require('express-session');
+const sessionStore = require('./config/sessionStore');
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.use(session({
+    key: 'swt',
+    secret: process.env.SESSION_SECRET,
+    store: sessionStore,
+    resave: false,
+    saveUninitialized: true, // setting this to true creates a cookie and session before anyone is logged in
+    cookie: {
+        maxAge: 2592000000, // maxAge: 2592000000 is 30 days
+    },
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.get('/pdf/:filename', function (req, res) {
     res.sendFile(path.join(__dirname, 'pdf/' + req.params.filename));
@@ -31,6 +48,9 @@ connectionPool.mysqlConnect()
                 res.sendFile(path.join(__dirname, './client/build/index.html'));
             });
         }
+        app.get('/', (req, res) => {
+            console.log(req.session);
+        });
         app.listen(PORT, () => {
             console.log('Server is listening on port ' + PORT);
         });
