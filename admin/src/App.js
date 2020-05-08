e93c89eabf21cebed0ef98e5626c49eca79db811
mike14747 <mike4747@oh.rr.com>
2020-05-07 23:40:02 -0400

    Added UserContext to the admin react app. Made a login page and added a UserContext.Provider to app.js

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import AdminCentral from './pages/adminCentral/adminCentral';
+import Login from './pages/login';
+import './css/my_style.css';
+import UserContext from './context/userContext';
 
 function App() {
+    const [user, setUser] = useState(null);
     return (
-        <div>
-            Admin Central
-        </div>
+        <UserContext.Provider value={[user, setUser]}>
+            <Router>
+                <Switch>
+                    <Route exact path="/" component={AdminCentral} />
+                    <Route path='/login' component={Login} />
+                </Switch>
+            </Router>
+        </UserContext.Provider>
     );
 }
 
---
+++ admin/src/pages/adminCentral/adminCentral.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from 'react';
+
+const AdminCentral = () => {
+    return (
+        <Fragment>
+            <h1>This is the Admin Central Page</h1>
+        </Fragment>
+    );
+};
+
+export default AdminCentral;
