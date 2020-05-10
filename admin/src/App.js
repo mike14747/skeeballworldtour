1eeb87f5e8ca81c0cae6f475763e33a56c1437a4
mike14747 <mike4747@oh.rr.com>
2020-05-10 01:30:55 -0400

    Moved admin user status logic to inside App.js.

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,14 +1,26 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 import AdminCentral from './pages/adminCentral/adminCentral';
 import Login from './pages/login/login';
 import './css/my_style.css';
 import UserContext from './context/userContext';
 import ProtectedRoute from './components/protectedRoute/protectedRoute';
-import UserStatus from './components/userStatus/userStatus';
+import axios from 'axios';
 
 function App() {
     const [user, setUser] = useState(null);
+
+    useEffect(() => {
+        axios.get('/api/auth/status')
+            .then(response => {
+                if (response.status === 200) {
+                    setUser(response.data.user);
+                } else {
+                    setUser(null);
+                }
+            })
+            .catch(error => console.log(error));
+    }, [setUser]);
 
     return (
         <Router>
@@ -18,7 +30,6 @@ function App() {
                 </ul>
             </nav>
             <UserContext.Provider value={[user, setUser]}>
-                <UserStatus />
                 <Switch>
                     <ProtectedRoute exact path="/" component={AdminCentral} user={user} />
                     <Route path='/login' component={Login} />
