c8c5cdeff655e22743dacc02f7c224f15bb06a85
mike14747 <mike4747@oh.rr.com>
2020-06-02 22:58:03 -0400

    Commented out /build in admin .gitignore.

---
+++ admin/build/precache-manifest.a16cd1586dabc7688fcea7615589be69.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7f761897544766b0da03c9b24976906",
+    "url": "/index.html"
+  },
+  {
+    "revision": "218368b3f6ac8cde006c",
+    "url": "/static/css/main.c46a0fb8.chunk.css"
+  },
+  {
+    "revision": "8cae77aee1267fdded1e",
+    "url": "/static/js/2.6dd2d6d0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.6dd2d6d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "218368b3f6ac8cde006c",
+    "url": "/static/js/main.372f6a58.chunk.js"
+  },
+  {
+    "revision": "d8c652b420ccdde8fd84",
+    "url": "/static/js/runtime-main.b9c8226d.js"
+  }
+]);
