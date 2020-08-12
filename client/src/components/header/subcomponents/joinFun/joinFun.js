c3bda688b22ca475888cffd7389d886a321ef6c6
mike14747 <mike4747@oh.rr.com>
2020-08-12 16:50:54 -0400

    Turned the joinFun.js component into a dropdown showing the text when you hover over the heading.

--- client/src/components/header/subcomponents/joinFun/joinFun.js
+++ client/src/components/header/subcomponents/joinFun/joinFun.js
@@ -1,20 +1,25 @@
-import React, { useContext } from 'react';
+import React, { useContext, Fragment } from 'react';
 import ReactHtmlParser from 'react-html-parser';
 import './css/joinFun.css';
 import SettingsContext from '../../../../context/settingsContext';
 
 const JoinFun = () => {
-    const settings = useContext(SettingsContext);
+    const { text_box_heading: textBoxHeading, text_box_text: textBoxText } = useContext(SettingsContext);
 
     return (
-        <div className="join">
-            {settings.text_box_heading &&
-                <div className="join-heading">{settings.text_box_heading}</div>
-            }
-            {settings.text_box_text &&
-                <div>{ReactHtmlParser(settings.text_box_text)}</div>
-            }
+        <div className="join-dropdown" data-toggle="dropdown">
+            <div className="join-heading">
+                {textBoxHeading &&
+                    <Fragment>{textBoxHeading} +</Fragment>
+                }
+            </div>
+            <div className="join-dropdown-content">
+                {textBoxText &&
+                    <Fragment>{ReactHtmlParser(textBoxText)}</Fragment>
+                }
+            </div>
         </div>
+
     );
 };
 
