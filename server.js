151a81469578824fca76e3c642a17f22aa5f0ee7
mike14747 <mike4747@oh.rr.com>
2020-09-10 12:48:22 -0400

    Decoupled admin area from this main app. Uninstalled all session/passport packages. Removed all auth code from server.js. Deleted passport folder. I still need to move the admin frontend folder to swt-cms, as well as some of the admin controllers and a few model methods.

--- server.js
+++ server.js
@@ -12,26 +12,12 @@ app.get('/pdf/:filename', (req, res) => {
     res.sendFile(path.join(__dirname, 'pdf/' + req.params.filename));
 });
 
-function checkAuthenticated(req, res, next) {
-    if (req.isAuthenticated()) {
-        return next();
-    } else {
-        return res.status(401).json({ message: 'User needs admin priviledges!' });
-        // return next();
-    }
-}
-
 const { dbTest } = require('./config/connectionPool');
 
 app.use(require('./controllers/testController'));
 
 dbTest()
     .then(() => {
-        app.use(require('./passport/expressSession'));
-        const passport = require('./passport/passportFunctions');
-        app.use(passport.initialize());
-        app.use(passport.session());
-        app.use('/api/admin', checkAuthenticated, require('./controllers/adminControllers'));
         app.use('/api', require('./controllers'));
     })
     .catch((error) => {
@@ -41,10 +27,6 @@ dbTest()
     })
     .finally(() => {
         if (process.env.NODE_ENV === 'production') {
-            // app.use(express.static(path.join(__dirname, 'admin/build')));
-            // app.get('/admin', (req, res) => {
-            //     res.sendFile(path.join(__dirname, 'admin/build/index.html'));
-            // });
             app.use(express.static(path.join(__dirname, 'client/build')));
             app.get('*', (req, res) => {
                 res.sendFile(path.join(__dirname, 'client/build/index.html'));
